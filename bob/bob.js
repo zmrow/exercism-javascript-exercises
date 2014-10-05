8c398e5e4df778af60de505c6a0354e392348aa6
Zac Mrowicki <zmrowicki@hotmail.com>
2014-10-05 10:57:26 -0700

    Initial commit

---
+++ bob/bob.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  switch(input) {
+    case "Tom-ay-to, tom-aaaah-to.":
+      console.log('Whatever.');
+      return "Whatever.";
+      break;
+  }
+};
+
+
+module.exports = Bob;
